0cb10202970e998db40264c0a54a7584cb701ed2
alemanjosh7 <josuealeman2003@gmail.com>
2024-04-12 17:50:11 -0600

    Update pearblock and cli querys

--- pearblock.js
+++ pearblock.js
@@ -1,12 +1,18 @@
+// #!/usr/bin env bare
 import fsp from 'bare-fs/promises'
 import Hyperswarm from 'hyperswarm'
 import Corestore from 'corestore'
 import Hyperbee from 'hyperbee'
 import b4a from 'b4a'
 import process from 'bare-process'
-async function pearblocks(corekey, word) {
+if (process.argv.includes('--h') || process.argv.includes('--help')) {
+  console.log(process.argv)
+  process.exit(-1)
+}
+async function ask(corekey, word) {
   const store = new Corestore(Pear.config.storage)
-
+  console.log(`Your core key is: ${corekey}`)
+  console.log(`The Pear Block you want to know about is: ${word}`)
   const swarm = new Hyperswarm()
   Pear.teardown(() => swarm.destroy())
 
@@ -24,13 +30,12 @@ async function pearblocks(corekey, word) {
   const discovery = swarm.join(core.discoveryKey)
 
   discovery.flushed().then(() => {
-    console.log('bee key:', b4a.toString(core.key, 'hex'))
+    console.log('You can connect from another command like using this Hyperbee key:', b4a.toString(core.key, 'hex'))
   })
 
   const key = b4a.toString(core.key, 'hex')
-
   if (core.length <= 1) {
-    console.log('importing dictionary...')
+    console.log('Importing dictionary...')
     const dict = JSON.parse(await fsp.readFile('./dict.json'))
     const batch = bee.batch()
     for (const { key, value } of dict) {
@@ -38,33 +43,31 @@ async function pearblocks(corekey, word) {
     }
     await batch.flush()
   } else {
-    console.log('seeding dictionary...')
+    console.log('Seeding dictionary...')
   }
 
   if (!key) throw new Error('provide a key')
 
-  // console.log('core key here is:', core.key.toString('hex'))
-
   swarm.join(core.discoveryKey)
 
-  // const stdin = new Pipe(0)
-
-  // stdin.on('data', (data) => {
-  // const word = data.toString().trim()
   if (!word.length) return
-  // console.log(word)
   let response = ''
+
   await bee.get(word).then(node => {
-    // console.log(node)
     if (!node || !node.value) {
       response = `No dictionary entry for ${word}`
       return
     }
-    response = `${word} is: ${node.value}`
+    response = `Holepunch's Block ${word} is: ${node.value}`
   })
 
   return await response
 }
-// insert in the first parameter they core key
-const beekey = await pearblocks('', 'hyperswarm')
-console.log(beekey)
+
+
+if (process.argv[2] == '--ask') {
+  const text = [process.argv[3].substring(2, process.argv[3].length), process.argv[4].substring(2, process.argv[4].length)]
+  const response = await ask(text[0], text[1])
+  console.log(response)
+  process.kill()
+}
