24e51afdc0802ce1df1c5948790dd43f389983fe
alemanjosh7 <josuealeman2003@gmail.com>
2024-05-02 21:49:33 -0600

    Add pearblock.js testing with brittle unit testing and create Corestore dir

--- pearblock.js
+++ pearblock.js
@@ -6,19 +6,19 @@ import b4a from 'b4a'
 import readline from 'bare-readline'
 import tty from 'bare-tty'
 
-const cmd = readline.createInterface({
+export const cmd = readline.createInterface({
   input: new tty.ReadStream(0),
   output: new tty.WriteStream(1)
 })
 
-cmd.input.setMode(tty.constants.MODE_RAW)
+export const executeCmd = cmd.input.setMode(tty.constants.MODE_RAW)
 
-const notFound = (command) => {
-  console.log(`\nerror: unknown command '${command}'`)
+export const notFound = (command) => {
+  return `\nerror: unknown command '${command}'`
 }
 
 const missingArgument = (...args) => {
-  console.log(`\nerror: missing argument '${args}'`)
+  return `\nerror: missing argument '${args}'`
 }
 
 const cmdOpt = (line) => {
@@ -29,9 +29,8 @@ const cmdOpt = (line) => {
   switch (line[1]) {
     case 'init':
       if (k) {
-        missingArgument(err)
-      }
-      else {
+        console.log(missingArgument(err))
+      } else {
         init(line[2])
       }
       break
@@ -47,15 +46,13 @@ const cmdOpt = (line) => {
     default:
       notFound()
   }
+}
 
-};
-
-cmd.on('data', lines => {
+export const help = cmd.on('data', lines => {
   if (lines.length < 10 || lines.substring(0, 10) !== 'pearblocks') {
     notFound()
-  }
-  else {
-    const line = lines.split(" ")
+  } else {
+    const line = lines.split(' ')
     if (line.length > 1) {
       if (line[1].substring(0, 2) == '--') {
         if (line[1].includes('help')) {
@@ -66,25 +63,20 @@ Commands:\n
   init <key>               Starts importing or seeding Pearblocks Dictionary with a specific Hypercore Key\n
   beekey <key>             With a Hypercore key you can ask what is your Hyperbee Key\n
   ask <word> <key>         Ask about a specific word in the dictionary with a specific Hypercore key\n`)
+        } else {
+          console.log(notFound())
         }
-        else {
-          notFound()
-        }
+      } else {
+        console.log(cmdOpt(line))
       }
-      else {
-        cmdOpt(line)
-      }
-    }
-    else notFound()
+    } else notFound()
   }
   cmd.prompt()
-
 })
 cmd.prompt()
-const beekey = async (corekey) => {
-  const store = new Corestore(Pear.config.storage)
+export const beekey = async (corekey) => {
+  const store = new Corestore('./reader-storage')
   const swarm = new Hyperswarm()
-  Pear.teardown(() => swarm.destroy())
 
   swarm.on('connection', conn => store.replicate(conn))
 
@@ -98,21 +90,20 @@ const beekey = async (corekey) => {
   await core.ready()
 
   const discovery = swarm.join(core.discoveryKey)
-
+  let answer = ''
   discovery.flushed().then(() => {
-    console.log('You can connect from another command like using this Hyperbee key:', b4a.toString(core.key, 'hex'), '\n')
+    answer = 'You can connect from another command like using this Hyperbee key:', b4a.toString(core.key, 'hex') + '\n'
   })
 
   await core.close()
   await store.close()
-
+  console.log(answer)
+  return answer
 }
 
-const init = async (corekey) => {
-  const store = new Corestore(Pear.config.storage)
+export const init = async (corekey) => {
+  const store = new Corestore('./reader-storage')
   const swarm = new Hyperswarm()
-  Pear.teardown(() => swarm.destroy())
-
   swarm.on('connection', conn => store.replicate(conn))
 
   const core = store.get({ name: corekey })
@@ -142,10 +133,9 @@ const init = async (corekey) => {
   await store.close()
 }
 
-const ask = async (word, corekey) => {
-  const store = new Corestore(Pear.config.storage)
+export const ask = async (word, corekey) => {
+  const store = new Corestore('./reader-storage')
   const swarm = new Hyperswarm()
-  Pear.teardown(() => swarm.destroy())
   swarm.on('connection', conn => store.replicate(conn))
   console.log(`The pearblock you want to know about is: ${word}`)
   const core = store.get({ name: corekey })
@@ -167,13 +157,12 @@ const ask = async (word, corekey) => {
   await bee.get(word).then(node => {
     if (!node || !node.value) {
       response = `No dictionary entry for ${word}\n`
-      return
+      return response
     }
     response = `Holepunch's Block ${word} is: ${node.value}\n`
   })
   console.log(response)
   await core.close()
   await store.close()
-
+  return response
 }
-
--- test/app.test.js
+++ test/app.test.js
@@ -1,83 +1,49 @@
 import test from 'brittle'
-import Corestore from 'corestore'
+import { beekey, notFound, init, cmd, help } from '../pearblock.js'
 import process from 'bare-process'
-import b4a from 'b4a'
-import RAM from 'random-access-memory'
-import fsp from 'bare-fs/promises.js'
 
-test('is dictionary key and value strings', async function (t) {
-  const dict = JSON.parse(await fsp.readFile('./dict.json'))
-  for (const { key, value } of dict) {
-    t.is(typeof (key), 'string')
-    t.is(typeof (value), 'string')
-  }
+test('beekey', async function (t) {
+  t.is(typeof (process.argv[2]), 'string')
+  setTimeout(() => {
+    beekey(process.argv[2])
+    process.exit()
+  }, 1000)
 })
 
-test('check if a corestore is not writtable', async function (t) {
-  const store = new Corestore(Pear.config.storage)
-  const core = store.get({ key: b4a.from(process.argv[2].substring(2, process.argv[2].length), 'hex') })
-  await core.ready()
-  store.close()
-  t.is(core.writable, false)
-  core.close()
+test('init', async function (t) {
+  t.ok(process.argv.length > 2)
+  t.is(typeof (process.argv[2]), 'string')
+  setTimeout(() => {
+    t.is(typeof (init(process.argv[2]), 'string'))
+    process.exit()
+  }, 1000)
+  t.pass('import initialized correctly')
 })
 
-test('does finding peers gives timeout', async function (t) {
-  const store = new Corestore(Pear.config.storage)
-  const core = store.get({ key: b4a.from(process.argv[2].substring(2, process.argv[2].length), 'hex') })
-  await core.ready()
-  const peers = t.test('find all peers')
-  peers.plan(1)
-  const foundPeers = store.findingPeers()
-  store.close()
-  setTimeout(() => peers.ok(foundPeers), 1000)
-  core.close()
+test('commandNotFound', async function (t) {
+  t.is(typeof (notFound('test')), 'string')
+  t.pass()
 })
 
-test('basic get with hex key', async function (t) {
-  const store = new Corestore(RAM)
-  const hexKey = 'a'.repeat(64)
-
-  const core = store.get(hexKey)
-  await core.ready()
-  t.is(b4a.toString(core.key, 'hex'), hexKey)
+test('ask', async function (t) {
+  t.ok(process.argv.length > 3)
+  t.is(typeof (process.argv[2]), 'string')
+  t.is(typeof (process.argv[3]), 'string')
+  setTimeout(() => {
+    t.is(typeof (ask(process.argv[2], process.argv[3]), 'string'))
+    process.exit()
+  }, 1000)
+  t.pass('question answered correctly')
 })
 
-test('basic get with with explicit hex key', async function (t) {
-  const store = new Corestore(RAM)
-  const hexKey = 'a'.repeat(64)
-
-  const core = store.get({ key: hexKey })
-  await core.ready()
-  t.is(b4a.toString(core.key, 'hex'), hexKey)
+test('cmdStdout', async function (t) {
+  t.plan(1)
+  cmd.output.on('close', () => t.pass('closed')).end('succesful')
 })
 
-test('storage locking', async function (t) {
-  const store1 = new Corestore(Pear.config.storage)
-  store1.ready()
-  const store2 = new Corestore(Pear.config.storage)
-  try {
-    await store2.ready()
-    t.fail('dir should have been locked')
-  } catch {
-    t.pass('dir was locked')
-  }
-
-  await store1.close()
-})
-
-test('session get after closing it', async function (t) {
-  const store = new Corestore(Pear.config.storage)
-
-  const session = store.session()
-  await session.close()
-
-  try {
-    session.get({ key: b4a.from(process.argv[2].substring(2, process.argv[2].length), 'hex') })
-    t.fail('Should have failed')
-  } catch (err) {
-    t.is(err.message, 'The corestore is closed')
-  }
-
-  await store.close()
+test('cmdHelp', async function (t) {
+  setTimeout(() => {
+    help()
+    process.exit()
+  }, 1000)
 })
